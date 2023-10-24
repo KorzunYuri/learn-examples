3a21d76e29ad2d2ae9fbdd996cef8b8739f550f3
KorzunYuri <korzun.yuri@gmail.com>
2023-10-24 14:24:38 +0200

    jmh-benchmarking dead code elimination examples

---
+++ jmh-benchmarking/src/main/java/com/yuryukorzun/learn/java/benchmarking/jmh/benchmark/WorkingBenchmark.java
@@ -0,0 +1,19 @@
+package com.yuryukorzun.learn.java.benchmarking.jmh.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+public class WorkingBenchmark extends MyBenchmark{
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @Fork(value = 1, warmups = 2)
+    @Timeout(time = 1)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void actualBenchmark(Blackhole blackhole) {
+        blackhole.consume(createAndReturnObject());
+    }
+
+}
